3aefafa61052f2310deb45edbc885774e5afa579
faizan2301 <skfaizan2301@gmail.com>
2023-08-10 16:56:35 +0530

    Some changes

--- src/Redux/slice/apiSlice.js
+++ src/Redux/slice/apiSlice.js
@@ -1,5 +1,6 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
-const baseUrl = 'http://192.168.1.25:3000/';
+// const baseUrl = 'http://192.168.1.25:3000/';
+const baseUrl = 'https://nikeappbackend.onrender.com/';
 
 // Define a service using a base URL and expected endpoints
 export const apiSlice = createApi({
